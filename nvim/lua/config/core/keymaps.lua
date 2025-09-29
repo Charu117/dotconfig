6cb4bc4b15c95662125fdcd1f6d748aecd606c3e
Charu Fernando <charufernando@Charus-MBP.homenet.telecomitalia.it>
2025-09-29 19:14:17 +0200

    fix: fix keymaps

--- nvim/lua/config/core/keymaps.lua
+++ nvim/lua/config/core/keymaps.lua
@@ -1,13 +1,12 @@
-vim.g.mapleader = " " 
+vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk"})
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights"})
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
-
 
 -- window management
 
